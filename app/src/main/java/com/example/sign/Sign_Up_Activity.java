aaf288978bd182f3136d147fafd415a400e86466
RemKouhai <iloveannie2325@gmail.com>
2024-11-05 12:58:28 +0700

    update activities.xml

--- app/src/main/java/com/example/sign/Sign_Up_Activity.java
+++ app/src/main/java/com/example/sign/Sign_Up_Activity.java
@@ -1,6 +1,7 @@
 package com.example.sign;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +11,25 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class Sign_Up_Activity extends AppCompatActivity {
 
+    private Button buttonSignUp;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_sign_up);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        buttonSignUp = findViewById(R.id.buttonSignUp);
+        buttonSignUp.setOnClickListener(v -> {
+            // Chuyển sang Sign_In_Activity
+            Intent intent = new Intent(Sign_Up_Activity.this, Sign_In_Activity.class);
+            startActivity(intent);
         });
     }
 }
